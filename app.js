c2880a21f36981f5a2939e95ff6b4fd624cb3d09
Trithuan <36200155+Trithuan@users.noreply.github.com>
2021-12-15 21:54:46 +0100

    caca

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Test push pour voir ça sert à quoi le pipeline !'));
+app.get('/', (req, res) => res.send('caca'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
